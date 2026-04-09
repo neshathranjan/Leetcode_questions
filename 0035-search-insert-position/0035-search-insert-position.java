e4b5884d7b3ef24b10cfd0d1b7d49de96c9554d9
Neshath Ranjan V <neshathchannel2024@gmail.com>
2026-04-09 16:13:53 +0530

    Time: 0 ms (100%), Space: 44.4 MB (93.63%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int size = nums.length;
+        int l=0,h=size-1;
+        while (l<=h) {
+            int mid = l+(h-l)/2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if(nums[mid]>target){
+                h = mid-1;
+            }
+            else{
+                l =mid+1;
+            }
+        }
+
+        return l;
+    }
+}
