69b979a49ae1beb2a82e21c999b6ed0258e08703
Neshath Ranjan V <neshathchannel2024@gmail.com>
2026-04-09 16:25:23 +0530

    Time: 0 ms (100%), Space: 43.1 MB (22.62%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String toLowerCase(String s) {
+        char[] ch =s.toCharArray();
+        for(int i=0;i<ch.length;i++){
+            if(ch[i]>=65 && ch[i]<=90){
+                ch[i]+=32;
+            }
+        }
+        return String.valueOf(ch);
+    }
+}
