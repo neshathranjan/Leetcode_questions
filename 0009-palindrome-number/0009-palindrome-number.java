adbaf502301dd587be7c3c8950c2b6aa86d513d4
Neshath Ranjan V <neshathchannel2024@gmail.com>
2026-04-09 16:11:58 +0530

    Time: 5 ms (79.44%), Space: 45.8 MB (78.48%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;//check the number is negative
+        int reverse = 0;
+        int copy = x;
+        while(x>0){
+        reverse = x % 10+ reverse*10;
+        x/=10;
+        }
+        return reverse == copy;
+    }
+}
