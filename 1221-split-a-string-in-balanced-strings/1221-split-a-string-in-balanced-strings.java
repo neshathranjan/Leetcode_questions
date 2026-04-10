6540b5a32a5f9444fe0acd2f5bd95326e272c902
Neshath Ranjan V <neshathchannel2024@gmail.com>
2026-04-10 09:21:09 +0530

    Time: 0 ms (100%), Space: 42.9 MB (13.26%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int balance = 0;
+        int ans = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == 'L') {
+                balance++;
+            } else {
+                balance--;
+            }
+            if (balance == 0) {
+                ans++;
+            }
+
+        }
+
+        return ans;
+    }
+}
